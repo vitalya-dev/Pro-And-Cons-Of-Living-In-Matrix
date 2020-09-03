a5b5b9140c0cbb30d484549558fec6dc982dd582
vitalya-dev <vitalya.dev@gmail.com>
2020-09-03 09:24:34 +0300

    Working On Buttons.

--- commander.py
+++ commander.py
@@ -3,18 +3,42 @@ from pygame.locals import *
 
 SCREEN_SIZE = (640, 480)
 
-
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
 
+screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
-background = pygame.surface.Surface(SCREEN_SIZE).convert()
-background.fill(pygame.Color('#0000a8'))
+def button(text):
+  btn = pygame.Surface((96, 32))
+  btn.fill(pygame.Color('#57ffff'))
+  return btn
 
+
+main_window = pygame.surface.Surface(SCREEN_SIZE).convert()
+main_window.fill(pygame.Color('#0000a8'))
+
+pathbar = pygame.surface.Surface((SCREEN_SIZE[0], 32)).convert()
+pathbar.fill(pygame.Color('#fefe03'))
+
+#================================================================#
+help_button = button(help)
+#================================================================#
+
+#================================================================#
+buttonbar = pygame.surface.Surface((SCREEN_SIZE[0], 32)).convert()
+buttonbar.fill(pygame.Color('#000000'))
+buttonbar.blit(help_button, (32, 0))
+#================================================================#
+
+#================================================================#
+main_window.blit(pathbar, (0, 0))
+main_window.blit(buttonbar, (0, SCREEN_SIZE[1] - 32))
+#================================================================#
 
 
 while True:
+  dt = clock.tick(60)
+  #PROCESS
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
@@ -22,8 +46,14 @@ while True:
     if event.type == KEYDOWN:
       pygame.quit()
       quit()
-  screen.blit(background, (0, 0))
+  #RENDER
+  screen.fill((0, 0, 0))
+  screen.blit(main_window, (0, 0))
+  #UPDATE
   pygame.display.update()
 
-  dt = clock.tick(60)
+
+
+
+
 
