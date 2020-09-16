1b4f7f630bd00d2650d575cbb2675fd9cc8ce4e3
vitalya-dev <vitalya.dev@gmail.com>
2020-09-16 09:32:12 +0300

    Working On Cursor

--- basic.py
+++ basic.py
@@ -31,6 +31,11 @@ def done(v=None):
   
 def dim_in_pixels(cols, rows):
   return (cols * (int)(FONT_SIZE / 2), rows * FONT_SIZE)
+
+def clamp(val, min, max):
+  if val < min: return min
+  if val > max: return max
+  return val
 #================================================================#
 
 
@@ -45,6 +50,15 @@ def button(text):
 
 def label(text, color):
   return font.render(text, False, pygame.Color(color))
+
+def cursor(color):
+  if not hasattr(cursor, 'blink'): cursor.blink = False
+  #==========#
+  cursor.blink = not cursor.blink
+  if cursor.blink:
+    return font.render("_", False, color)
+  else:
+    return font.render("",  False, color)
 #================================================================#
 
 
@@ -83,36 +97,64 @@ class Editor(Window):
     super().render()
     for i, line in enumerate(self.text):
       self.surface.blit(font.render("".join(line), False, pygame.Color('#c0c0c0')), dim_in_pixels(0, i))
+    self.surface.blit(cursor(pygame.Color('#c0c0c0')), dim_in_pixels(self.x, self.y))
     return self.surface
 
   def on_key_down(self, e):
     if e.key == K_ESCAPE:
       done(True)
-    if e.key == K_SPACE:
+    elif e.key == K_SPACE:
       self.text[self.y] += ' '
-      cursor_right()
-    if e.key == K_BACKSPACE and self.x > 0:
+      self.cursor_right()
+    elif e.key == K_BACKSPACE and self.x > 0:
       del(self.text[self.y][self.x-1])
-      cursor_left()
-    if e.key == K_BACKSPACE and self.x == 0:
-      merge_line(self.y-1, self.y)
-      del(self.text[self.y])
+      self.cursor_left()
+    elif e.key == K_BACKSPACE and self.x == 0:
+      self.cursor_up()
+      self.cursor_end_of_line()
       #=======#
-      cursor_up()
-      cursor_beg_of_line()
-    if e.key == K_RETURN:
+      self.merge_lines(self.y, self.y+1)
+    elif e.key == K_RETURN:
       text_0 = self.text[self.y][0:self.x]
       text_1 = self.text[self.y][self.x:]
       #=======#
       self.text[self.y] = text_0
       self.text.insert(self.y+1, text_1)
       #=======#
-      cursor_down()
-      cursor_end_of_line()
-    if e.unicode.isalpha():
+      self.cursor_down()
+      self.cursor_beg_of_line()
+    elif e.unicode.isalpha():
       self.text[self.y] += e.unicode
-      cursor_right()
+      self.cursor_right()
+
     print(self.text)
+
+  def cursor_up(self):
+    if self.y > 0:
+      self.y -= 1
+
+  def cursor_down(self):
+    if self.y < len(self.text)-1:
+      self.y += 1
+
+  def cursor_right(self):
+    if self.x in range(len(self.text[self.y])):
+      self.x += 1
+
+  def cursor_left(self):
+    if self.x > 0:
+      self.x -= 1
+
+  def cursor_beg_of_line(self):
+    self.x = 0
+  
+  def cursor_end_of_line(self):
+    self.x = len(self.text[self.y])
+
+  def merge_lines(self, y1, y2):
+    if y1 in range(len(self.text)) and y2 in range(len(self.text)):
+      self.text[y1] += self.text[y2]
+      del(self.text[y2])
 
 #================================================================#
 
