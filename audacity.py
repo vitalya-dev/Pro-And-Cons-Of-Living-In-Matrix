20ca7e56aef7387c682a46924ce606a096037f29
vitalya-dev <vitalya.dev@gmail.com>
2020-09-25 09:27:29 +0300

    Ok. Read Wav On My Own.

--- audacity.py
+++ audacity.py
@@ -1,6 +1,7 @@
+import struct
 import wave
 import sys
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 def linspace(start, stop, num=50):
@@ -8,6 +9,39 @@ def linspace(start, stop, num=50):
   div = num - 1
   step = delta / div
   return [start + i * step for i in range(num)]
+
+def read_whole(filename):
+    SIZES = {1: 'B', 2: 'h', 4: 'i'}
+    CHUNK_SIZE_4096 = 4096
+    CHUNK_SIZE_1 = 1
+    #======================#
+    wav_r = wave.open(filename, 'r')
+    #======================#
+    channels  = wav_r.getnchannels()
+    sampwidth = wav_r.getsampwidth()
+    #======================#
+    fmt_1 = "<" + SIZES[wav_r.getsampwidth()] * channels * CHUNK_SIZE_4096
+    fmt_2 = "<" + SIZES[wav_r.getsampwidth()] * channels * CHUNK_SIZE_1
+    #======================#
+    ret = []
+    while wav_r.tell() < wav_r.getnframes():
+      if wav_r.getnframes() - wav_r.tell() >= CHUNK_SIZE_4096:
+        decoded = struct.unpack(fmt_1, wav_r.readframes(CHUNK_SIZE_4096))
+      else:
+        decoded = struct.unpack(fmt_2, wav_r.readframes(CHUNK_SIZE_1))
+      for i in decoded: ret.append(i)
+    return ret
+
+
+# def __init__(self, filename, read=True, debug=False):
+#     mode = 'r' if read else 'w'
+#     sizes = {1: 'B', 2: 'h', 4: 'i'}
+#     self.wav = wave.open(filename, mode)
+#     ...
+#     self.channels = self.wav.getnchannels()
+#     self.fmt_size = sizes[self.wav.getsampwidth()]
+#     self.fmt = "<" + self.fmt_size * self.channels
+
   
 assert len(linspace(0, 2, 6)) == 6
 assert linspace(0, 2, 6)[5] == 2
@@ -15,7 +49,17 @@ assert linspace(0, 2, 6)[0] == 0
 
 print(linspace(0, 2, 6))
 
+print(len(read_whole("Live Ouside Instrumental 2.wav")))
+#print(len(np.frombuffer(wave.open("Live Ouside Instrumental 2.wav", "r").readframes(-1), "Int16")))
 
+
+#print(read_whole("Live Ouside Instrumental 2.wav"))
+# #w = wave.open("Live Ouside Instrumental 2.wav", "r")
+# data = w.readframes(1)
+
+# print(int.from_bytes(data, "little", signed=True))
+# print(struct.unpack("<h", data))
+# print(numpy.frombuffer(data, "Int16")[0])
 
 # w = wave.open("Live Ouside Instrumental 2.wav", "r")
 
@@ -58,3 +102,30 @@ print(linspace(0, 2, 6))
 # # plt.show()
 
 
+# def read_whole(filename):
+#     wav_r = wave.open(filename, 'r')
+#     ret = []
+#     while wav_r.tell() < wav_r.getnframes():
+#         decoded = struct.unpack("<h", wav_r.readframes(1))
+#         ret.append(decoded)
+#     return ret
+
+# def read_whole(filename):
+#   wav_r = wave.open(filename, 'r')
+#   ret = []
+#   while wav_r.tell() < wav_r.getnframes():
+#     decoded = int.from_bytes(wav_r.readframes(1), "little", signed=True)
+#     ret.append(decoded)
+#   return ret
+
+
+#print(int.from_bytes(data, "little", signed=True))
+
+# def read_whole(filename):
+#   sizes = {1: 'B', 2: 'h', 4: 'i'}
+#   wav_r = wave.open(filename, 'r')
+#   ret = []
+#   while wav_r.tell() < wav_r.getnframes():
+#     decoded = int.from_bytes(wav_r.readframes(1), "little", signed=True)
+#     ret.append(decoded)
+#   return ret
