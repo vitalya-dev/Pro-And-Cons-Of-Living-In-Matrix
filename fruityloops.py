9f4694b7d9f9573ffa572fddf883db7a720c9031
vitalya-dev <vitalya.dev@gmail.com>
2020-10-06 08:08:46 +0300

    No Variables.

--- fruityloops.py
+++ fruityloops.py
@@ -12,6 +12,17 @@ from mido import MidiFile
 def notes(midifile):
   return [message for message in MidiFile(filename) if message.type == 'note_on' or message.type == 'note_off']
 
+def playback():
+  if not hasattr(playback, 'start'): playback.start = time.time()
+  return time.time() - playback.start
+
+def timing(note=None):
+  if not hasattr(timing, 'time'): timing.time = 0.0
+  if not hasattr(timing, 'last_note'): timing.last_note = None
+  #===============#
+  timing.time += note.time if note else 0.0
+  #===============#
+  return timing.time - playback()
 
 if __name__ == '__main__':
   filename = sys.argv[1]
@@ -21,12 +32,7 @@ if __name__ == '__main__':
       portname = None
 
   with mido.open_output(portname) as output:
-    start_time = time.time()
-    input_time = 0.0
     for note in notes(filename) * 10:
-      input_time += note.time
-      playback_time = time.time() - start_time
-      if input_time - playback_time > 0.0: time.sleep(input_time - playback_time)
+      if timing(note) > 0.0: time.sleep(timing())
       output.send(note)
-      print(note.dict())
 
