8f50e6590ad2d1b73a17c1b2e8b98a332cea1e5a
vitalya-dev <vitalya.dev@gmail.com>
2020-10-06 09:29:47 +0300

    Start Working On Plotting.

--- fruityloops.py
+++ fruityloops.py
@@ -4,17 +4,20 @@ Play MIDI file on output port.
 Run with (for example):
     ./play_midi_file.py 'SH-201 MIDI 1' 'test.mid'
 """
+import time
 import sys
 import mido
-import time
-from mido import MidiFile
+
+import pygame
+from pygame.locals import *
 
 def notes(midifile):
-  return [message for message in MidiFile(filename) if message.type == 'note_on' or message.type == 'note_off']
+  return [message for message in mido.MidiFile(filename) if message.type == 'note_on' or message.type == 'note_off']
 
 def playback():
   if not hasattr(playback, 'start'): playback.start = time.time()
   return time.time() - playback.start
+
 
 def timing(note=None):
   if not hasattr(timing, 'time'): timing.time = 0.0
@@ -23,6 +26,22 @@ def timing(note=None):
   timing.time += note.time if note else 0.0
   #===============#
   return timing.time - playback()
+
+
+#================================================================#
+class Plot(object):
+  def __init__(self, cols, rows, background, foreground):
+    self.cols = cols
+    self.rows = rows
+    self.background = pygame.Color(background)
+    self.foreground = pygame.Color(foreground)
+
+  def plot(self, data):
+    surface = pygame.surface.Surface(dim_in_pixels(self.cols, self.rows)).convert()
+    surface.fill(self.background)
+    return surface
+#================================================================#
+
 
 if __name__ == '__main__':
   filename = sys.argv[1]
@@ -35,4 +54,5 @@ if __name__ == '__main__':
     for note in notes(filename) * 10:
       if timing(note) > 0.0: time.sleep(timing())
       output.send(note)
+      print(note)
 
