9cf94b16836e8711a76391c370ae1159b8f1a4ab
vitalya-dev <vitalya.dev@gmail.com>
2020-11-02 09:34:26 +0300

    Working On Particles System.

--- nutcracker.py
+++ nutcracker.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame.locals import *
 
@@ -51,18 +53,20 @@ class Keyboard(object):
       if e.type == KEYDOWN and e.key == K_SPACE:
         for c in self.on_space: c()
 
-class Progression:
-  def __init__(self, prog):
-    self.progression = progression
-    self._i = 0
+class Particles:
+  def __init__(self, timer):
+    self.surface = pygame.surface.Surface(SCREEN_SIZE).convert()
+    self.surface.set_colorkey((0, 0, 0))
 
-  @property
-  def current(self):
-    return self.progression[self._i]
+  def generate(self, n):
+    for i in range(0, n):
+      pygame.draw.circle(self.surface, (255, 255, 255), self.sruface.get_rect().center, 10 * random.random())
 
-  def next(self):
-    self._i = (self._i + 1) % len(self.progression)
+  def process(self, events):
+    pass
 
+  def render(self):
+    return self.surface
 
 class Framesheet(object):
   def __init__(self, *frames):
@@ -123,21 +127,25 @@ if __name__ == '__main__':
   #================#
   mr_pleasant_frames = Framesheet("graphics/mr_pleasant_1.png", "graphics/mr_pleasant_2.png", "graphics/mr_pleasant_2.png").scale(14)
   mr_pleasant_positions = [
-    subtract(subtract(screen.get_rect().center, mr_pleasant_frames[0].get_rect().center), (0, 75)),
-    subtract(subtract(screen.get_rect().center, mr_pleasant_frames[1].get_rect().center), (0, 75)),
-    subtract(subtract(screen.get_rect().center, mr_pleasant_frames[1].get_rect().center), (0, 75))
+    subtract(screen.get_rect().center, (0, 75)),
+    subtract(screen.get_rect().center, (0, 75)),
+    subtract(screen.get_rect().center, (0, 75))
   ]
   #================#
   nutcracker_frames  = Framesheet("r:180:graphics/nutcracker.png", "graphics/nutcracker.png", "r:180:graphics/nutcracker.png").scale(14)
   nutcracker_positions = [
-    subtract(subtract(screen.get_rect().center, nutcracker_frames[1].get_rect().center), (0, -125)),
-    subtract(subtract(screen.get_rect().center, nutcracker_frames[0].get_rect().center), (0, -45)),
-    subtract(subtract(screen.get_rect().center, nutcracker_frames[1].get_rect().center), (0, -125))
+    subtract(screen.get_rect().center, (0, -125)),
+    subtract(screen.get_rect().center, (0, -45)),
+    subtract(screen.get_rect().center, (0, -125))
   ]
+  #================#
+  particles = Particles(3)
+  particles_positions = [nutcracker_positions[0], nutcracker_positions[1], nutcracker_positions[2]]
   #================#
   keyboard = Keyboard()
   keyboard.on_space += [lambda: mr_pleasant_frames.next_frame()]
   keyboard.on_space += [lambda: nutcracker_frames.next_frame()]
+  keyboard.on_space += [lambda: particles.generate(250 * 1 if nutcracker_frames.index == 1 else 0)]
   keyboard.on_esc   += [lambda: done(True)]
   #================#
 
@@ -145,8 +153,10 @@ if __name__ == '__main__':
     #PROCESS INPUT
     events = pygame.event.get()
     keyboard.process(events)
+    particles.process(events)
     #RENDER
     screen.fill(pygame.Color('#000000'))
     screen.blit(mr_pleasant_frames.current, mr_pleasant_positions[mr_pleasant_frames.index])
     screen.blit(nutcracker_frames.current,  nutcracker_positions[nutcracker_frames.index])
+    screen.blit(particles.render(), particles_positions)
     pygame.display.update()
