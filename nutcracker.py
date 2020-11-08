0884588154102d9fed4507eea42d009034340def
vitalya-dev <vitalya.dev@gmail.com>
2020-11-08 18:07:39 +0300

    Got An Tuple Math.

--- nutcracker.py
+++ nutcracker.py
@@ -1,6 +1,6 @@
 import random
 import itertools
-from couple import Couple
+import operator
 
 import pygame
 from pygame.locals import *
@@ -29,18 +29,22 @@ def done(val=None):
   if val == None: return done.val
   done.val = val;
 
-def multiply(l1, l2):
-  from operator import mul
-  return tuple(map(mul, l1, l2))
-
-def to_int(l):
-  return tuple(map(int, l))
-
-def random_pair(a, b):
+def random_couple(a, b):
   return (random.randint(a, b), random.randint(a, b))
 
 def random_triple(a, b):
   return (random.randint(a, b), random.randint(a, b), random.randint(a, b))
+
+def tuple_math(a, op, b):
+  if not hasattr(b, "__getitem__"):
+    b = (b,) * len(a)
+  if op == '+':
+    return tuple(map(operator.add, a, b))
+  if op == '-':
+    return tuple(map(operator.sub, a, b))
+  if op == '*':
+    return tuple(map(operator.mul, a, b))
+
 #================================================================#
 
 
@@ -60,30 +64,33 @@ class Keyboard(object):
 class Particles:
   def __init__(self):
     self._particles = []
+    self.position = (0, 0)
+    self.count = 0
 
-  def generate(self, position, n):
-    for i in range(0, n):
+  def boom(self):
+    for i in range(0, self.count):
       self._particles.append({
-        'position' : position.copy(),
+        'position' : self.position,
         'radius'   : random.randint(0, 10),
-        'velocity' : Couple(*random_pair(-150, 150)),
+        'velocity' : random_couple(-150, 150),
         'color'    : random_triple(0, 255)
       })
 
   def process(self, events):
     for particle in self._particles:
-      particle['position'] += particle['velocity'] * clock.get_time() * 0.001
+      movement = tuple_math(particle['velocity'], '*', clock.get_time() * 0.001)
+      particle['position'] = tuple_math(particle['position'], '+', movement)
 
   def render(self, surface):
     for particle in self._particles:
-      pygame.draw.circle(surface, particle['color'], to_int(particle['position'].as_tuple()), particle['radius'])
+      pygame.draw.circle(surface, particle['color'], tuple(map(int, particle['position'])), particle['radius'])
 
 class Framesheet(object):
   def __init__(self, *frames):
     self.frames = [self._load(frame) if type(frame) == type(str()) else frame.copy() for frame in frames]
     self.frame_offsets = list(itertools.repeat((0, 0), len(self.frames)))
-    self.position = Couple(0, 0)
-    self.pivot = Couple(0, 0)
+    self.position = (0, 0)
+    self.pivot = (0, 0)
     self._current_frame_index = 0 
 
   def _load(self, frame_name):
@@ -102,7 +109,7 @@ class Framesheet(object):
     if translate_function == 's':
       translate_param = int(translate_param)
       frame = pygame.image.load(frame_name).convert_alpha()
-      return pygame.transform.scale(frame, multiply(frame.get_size(), (translate_param, translate_param)))
+      return pygame.transform.scale(frame, tuple_math(frame.get_size(), '*', translate_param))
     else:
       return pygame.image.load(frame_name)
 
@@ -124,16 +131,18 @@ class Framesheet(object):
 
   @property
   def frame_position(self):
-    return self.position - self.pivot * self.current_frame.get_size() + self.frame_offsets[self.current_index]
+    pivot_position = tuple_math(self.pivot, '*', self.current_frame.get_size())
+    top_left_corner_position = tuple_math(self.position, '-', pivot_position)
+    return tuple_math(top_left_corner_position, '+', self.frame_offsets[self.current_index])
 
   def next_frame(self):
     self._current_frame_index = (self._current_frame_index + 1) % len(self.frames)
 
   def render(self, surface):
-    surface.blit(self.current_frame, to_int(self.frame_position))
+    surface.blit(self.current_frame, tuple(map(int, (self.frame_position))))
 
   def scale(self, scale_factor):
-    return Framesheet(*[pygame.transform.scale(frame, multiply(frame.get_size(), (scale_factor, scale_factor))) for frame in self.frames])
+    return Framesheet(*[pygame.transform.scale(frame, tuple_math(frame.get_size(), '*', scale_factor)) for frame in self.frames])
 
   def __getitem__(self, index):
     return self.frames[index]
@@ -144,22 +153,23 @@ class Framesheet(object):
 #================================================================#
 
 if __name__ == '__main__':
-  #================#
   mr_pleasant = Framesheet("graphics/mr_pleasant_1.png", "graphics/mr_pleasant_2.png", "graphics/mr_pleasant_2.png").scale(14)
-  mr_pleasant.pivot = Couple(0.5, 0.5)
-  mr_pleasant.position = Couple(*screen.get_rect().center) + (0, -75)
+  mr_pleasant.pivot = (0.5, 0.5)
+  mr_pleasant.position = tuple_math(screen.get_rect().center, '+', (0, -75))
   #================#
   nutcracker = Framesheet("r:180:graphics/nutcracker.png", "graphics/nutcracker.png", "r:180:graphics/nutcracker.png").scale(14)
-  nutcracker.position  = Couple(*screen.get_rect().center) + (0, 125)
-  nutcracker.pivot = Couple(0.5, 0.5)
+  nutcracker.position  = tuple_math(screen.get_rect().center, '+', (0, 125))
+  nutcracker.pivot = (0.5, 0.5)
   nutcracker.frame_offsets = [(0, 0), (0, -80), (0, 0)]
   #================#
   particles = Particles()
+  particles.count = 25
+  particles.position = tuple_math(nutcracker.position, '+', (0, -150))
   #================#
   keyboard = Keyboard()
   keyboard.on_space += [lambda: mr_pleasant.next_frame()]
   keyboard.on_space += [lambda: nutcracker.next_frame()]
-  keyboard.on_space += [lambda: particles.generate(nutcracker.position + (0, -150), 25) if nutcracker.current_index == 1 else ...]
+  keyboard.on_space += [lambda: particles.boom() if nutcracker.current_index == 1 else ...]
   keyboard.on_esc   += [lambda: done(True)]
   #================#
 
