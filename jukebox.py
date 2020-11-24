fc5c75fbefb7fa6152c7ab590d5e893ddbc93e7e
vitalya-dev <vitalya.dev@gmail.com>
2020-11-24 09:37:32 +0300

    Working On Switch Panel.

--- jukebox.py
+++ jukebox.py
@@ -38,6 +38,8 @@ def tuple_math(a, op, b):
     return tuple(map(operator.sub, a, b))
   if op == '*':
     return tuple(map(operator.mul, a, b))
+  if op == '/':
+    return tuple(map(operator.truediv, a, b))
 #================================================================#
 
 
@@ -69,20 +71,28 @@ class HorizontalSelectorSwitchButton(object):
 
 
   @property
-  def button_rect(self):
+  def rect(self):
     return pygame.Rect(self.position, tuple_math(self._rendered_text.get_size(), '-', (0, 1)))
   
   @property
-  def button_size(self):
-    return self.button_rect.size
+  def size(self):
+    return self.rect.size
 
   @property
-  def button_height(self):
-    return self.button_rect.height
+  def height(self):
+    return self.rect.height
 
   @property
-  def button_width(self):
-    return self.button_rect.width
+  def width(self):
+    return self.rect.width
+
+  @property
+  def x(self):
+    return self.rect.x
+
+  @property
+  def y(self):
+    return self.rect.y
 
   def process(self, events):
     pass
@@ -93,18 +103,28 @@ class HorizontalSelectorSwitchButton(object):
     self._draw_text(surface)
 
   def _draw_background(self, surface):
-    surface.fill(self._background_color, self.button_rect)
+    surface.fill(self._background_color, self.rect)
 
   def _draw_light(self, surface):
-    light_position = tuple_math(self.position, '+', (0, self.button_height * 3 / 4))
-    light_size = (self.button_width, self.button_height  / 4)
+    light_position = tuple_math(self.position, '+', (0, self.height * 3 / 4))
+    light_size = (self.width, self.height  / 4)
     surface.fill(self._light_color, pygame.Rect(light_position, light_size))
 
   def _draw_text(self, surface):
     surface.blit(self._rendered_text, tuple_math(self.position, '-', (0, 5)))
     
 
-VerticalSelectorSwitchButton = HorizontalSelectorSwitchButton
+class VerticalSelectorSwitchButton(HorizontalSelectorSwitchButton):
+  def __init__(self, text):
+    super().__init__(text)
+    self._rendered_text = pygame.transform.rotate(self._rendered_text, -90)
+
+  def render(self, surface):
+    self._draw_background(surface)
+    self._draw_text(surface)
+
+  def _draw_text(self, surface):
+    surface.blit(self._rendered_text, self.position)
 
 #================================================================#
 
@@ -116,8 +136,14 @@ if __name__ == '__main__':
   keyboard = Keyboard()
   keyboard.on_esc += [lambda: done(True)]
   #================#
-  a_btn = HorizontalSelectorSwitchButton('A')
-  a_btn.position = screen.get_rect().center
+  selector_switch = []
+  for i, button_name in enumerate('ABCDEF123456'):
+    button = HorizontalSelectorSwitchButton(button_name)
+    button.position = tuple_math(screen.get_rect().center, '/', (4, 1))
+    if i > 0:
+      button.position = tuple_math(selector_switch[i-1].position, '+', (selector_switch[i-1].width, 0))
+      button.position = tuple_math(button.position, '+', (25, 0))
+    selector_switch.append(button)
 
   select_btn = VerticalSelectorSwitchButton("SELECT")
   select_btn.position = tuple_math(screen.get_rect().center, '+', (50, 50))
@@ -127,11 +153,11 @@ if __name__ == '__main__':
     #PROCESS INPUT
     events = pygame.event.get()
     keyboard.process(events)
-    a_btn.process(events)
     select_btn.process(events)
     #RENDER
     screen.fill(pygame.Color('#000000'))
-    a_btn.render(screen)
+    for button in selector_switch:
+      button.render(screen)
     select_btn.render(screen)
     pygame.display.update()
 
