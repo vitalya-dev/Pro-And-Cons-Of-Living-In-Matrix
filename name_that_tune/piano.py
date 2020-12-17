56f8f00679640c60ed7a72e78df3ae6ae361f194
vitalya-dev <vitalya.dev@gmail.com>
2020-12-17 07:47:02 +0300

    Piano Is Ready.

--- name_that_tune/piano.py
+++ name_that_tune/piano.py
@@ -1,11 +1,15 @@
 import mido
+import pygame
+from pygame.locals import *
 
+from midi import *
 from constants import *
 from utils import *
 
 class Piano(object):
-  def __init__(self, midioutput):
+  def __init__(self, midioutput, keys):
     self.midioutput = midioutput
+    self.keys = keys
 
   def process(self, events):
     keys_down = pygame.key.get_pressed()
@@ -21,9 +25,35 @@ class Piano(object):
   def on_key_up(self, key):
     self.midioutput.send(mido.Message('note_off', note=self.keys[key]))
 
+  @staticmethod
+  def generate_keys_from_midi(midi):
+    middle_note = midi.middle_note()
+    return {
+    'F': middle_note,
+    'D': middle_note-1,
+    'S': middle_note-2,
+    'A': middle_note-3,
+    'J': middle_note+1,
+    'K': middle_note+2,
+    'L': middle_note+3,
+  }
+    
+
+
 
 if __name__ == '__main__':
   midioutput = mido.open_output(None)
-  #===================#
-  print(read_midi_notes('Breath.mid'))
-  piano = Piano(midioutput)
+  piano = Piano(midioutput, Piano.generate_keys_from_midi(Midi('Breath.mid')))
+  #===========================================INIT=================================================#
+  pygame.init()
+  screen = pygame.display.set_mode(SCREEN_SIZE)
+  clock = pygame.time.Clock()
+  #================================================================================================#
+  while not done():
+    clock.tick()
+    #===========================================PROCESS=================================================#
+    events = pygame.event.get()
+    piano.process(events)
+    #===========================================RENDER==================================================#
+    screen.fill(pygame.Color('#000000'))
+    pygame.display.update()
