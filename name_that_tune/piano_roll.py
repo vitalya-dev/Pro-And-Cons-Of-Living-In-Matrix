34d78eac7006ec3e2c24b410e82ed8f673c36198
vitalya-dev <vitalya.dev@gmail.com>
2020-12-18 07:44:03 +0300

    Got An Piano Roll

--- name_that_tune/piano_roll.py
+++ name_that_tune/piano_roll.py
@@ -10,24 +10,23 @@ from utils import *
 from keyboard import *
 
 class PianoRoll(object):
-  def __init__(self, midioutput, beats):
+  def __init__(self, midioutput):
     self.midioutput = midioutput
-    self.beats = beats
     self._play = False
 
-  def toggle(self):
-    self._play = not self._play
-    if self._play:
-      threading.Thread(target=self._play_thread).start()
-
-  def play(self, beats):
+  def start_or_stop_playing_beats(self, beats=None):
     if not self._play:
       self._play = True
       threading.Thread(target=self._play_thread, args=(beats,)).start()
+    else:
+      self._play = False
+
+  def stop(self):
+    self._play = False
 
   def _play_thread(self, beats):
     start_time = time.time()
-    beats_stream = sorted(flatten(self.beats), key=lambda beat: beat.time)
+    beats_stream = sorted(flatten(beats), key=lambda beat: beat.time)
     for beat in beats_stream:
       playback_time = time.time() - start_time
       if beat.time - playback_time > 0:
@@ -45,10 +44,10 @@ if __name__ == '__main__':
   clock = pygame.time.Clock()
   #================================================================================================#
   midioutput = mido.open_output(None)
-  piano_roll = PianoRoll(midioutput, Midi('Breath.mid').beats())
+  piano_roll = PianoRoll(midioutput)
 
   keyboard = Keyboard()
-  keyboard.on_space += [lambda: piano_roll.toggle()]
+  keyboard.on_space += [lambda: piano_roll.start_or_stop_playing_beats(Midi('Breath.mid').beats())]
 
   while not done():
     clock.tick()
