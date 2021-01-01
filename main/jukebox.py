042c1d7fdab9f2c4e55a539773eb3b087ce4bfd4
vitalya-dev <vitalya.dev@gmail.com>
2021-01-01 20:29:39 +0300

    Ok. Got An Jukebox. Time To Make An HUD.

--- main/jukebox.py
+++ main/jukebox.py
@@ -11,13 +11,13 @@ from scroll import *
 
 
 class Jukebox(Shape):
-  def __init__(self, size, parent=None):
+  def __init__(self, song_entries, size, parent=None):
     super().__init__(parent)
     #================#
     self._surface = pygame.surface.Surface(size).convert()
     #================#
     self._song_selector = self._create_song_selector()
-    self._song_holder_scroller = self._create_song_holder_scroller()
+    self._song_holder_scroller = self._create_song_holder_scroller(song_entries)
     self._song_holder_scroller.move(0, 120)
     
 
@@ -39,15 +39,11 @@ class Jukebox(Shape):
       song_selector.add_selector(HorizontalButton(i)) 
     return song_selector
 
-  def _create_song_holder_scroller(self):
+  def _create_song_holder_scroller(self, song_entries):
     song_holder_scroller = Scroll(SongHolder())
     song_holder_scroller.limit = self._surface.get_size()
-    song_holder_scroller.child.add_song_entry(SongEntry('You Cant Always Get What You Want', 'A1'))
-    song_holder_scroller.child.add_song_entry(SongEntry('Sympathy For Devil', 'A2'))
-    song_holder_scroller.child.add_song_entry(SongEntry('Another Break In The Wall', 'A3'))
-    song_holder_scroller.child.add_song_entry(SongEntry('California Dreaming', 'B1'))
-    song_holder_scroller.child.add_song_entry(SongEntry('No Woman No Cry', 'B2'))
-    song_holder_scroller.child.add_song_entry(SongEntry('Voodoo Child', 'B3'))
+    for song_entry in song_entries:
+      song_holder_scroller.child.add_song_entry(song_entry)
     return song_holder_scroller
 
 
@@ -60,7 +56,14 @@ if __name__ == '__main__':
   #================================================================================================#
 
   #================#
-  jukebox = Jukebox(SCREEN_SIZE)
+  song_entries = []
+  song_entries.append(SongEntry('You Cant Always Get What You Want', 'A1'))
+  song_entries.append(SongEntry('Sympathy For Devil', 'A2'))
+  song_entries.append(SongEntry('Another Break In The Wall', 'A3'))
+  song_entries.append(SongEntry('California Dreaming', 'B1'))
+  song_entries.append(SongEntry('No Woman No Cry', 'B2'))
+  song_entries.append(SongEntry('Voodoo Child', 'B3'))
+  jukebox = Jukebox(song_entries, SCREEN_SIZE)
   #================#
 
   while not done():
