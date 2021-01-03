b614ecd8949921436f8096825c4b435e6a36b0fe
vitalya-dev <vitalya.dev@gmail.com>
2021-01-03 14:06:27 +0300

    Got An SwitchButtons.

--- main/vertical_switch.py
+++ main/vertical_switch.py
@@ -1,20 +1,19 @@
-from horizontal_button import *
+from horizontal_switch  import *
 
 
-class HorizontalSwitch(HorizontalButton):
+class VerticalSwitch(HorizontalSwitch):
   def __init__(self, text, background_color=BLACK, text_color=GRAY, parent=None):
     super().__init__(text, background_color, text_color, parent)
+    self.padding = (2, 0)
+    self.rotate(-90)
 
-  def process(self, events):
-    pass
-
-  def toggle(self):
-    self._clicked = not self._clicked
-
-
-        
-
-
+  def draw(self):
+    self._draw_background()
+    self._draw_text()
+    if self._clicked:
+      self._draw_highlight()
+    return self._surface
+  
 
 if __name__ == '__main__':
   #===========================================INIT=================================================#
@@ -24,19 +23,18 @@ if __name__ == '__main__':
   clock = pygame.time.Clock()
   #================================================================================================#
 
-  a_switch = HorizontalSwitch('A')
-  a_switch.pivot = (0, 0)
-  a_switch.position = screen.get_rect().center
+  select_switch = VerticalSwitch('SELECT')
+  select_switch.pivot = (0.5, 0.5)
+  select_switch.position = screen.get_rect().center
 
   while not done():
     clock.tick()
     #===========================================PROCESS=================================================#
     events = pygame.event.get()
-    a_switch.process(events)
+    select_switch.process(events)
     #===========================================RENDER==================================================#
     screen.fill((0, 0, 0))
-    screen.blit(a_switch.draw(), a_switch.world_space_rect.topleft)
+    screen.blit(select_switch.draw(), select_switch.world_space_rect.topleft)
 
     pygame.display.update()
-
 
